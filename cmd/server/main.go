05b32423bc6fb9a4cd52040cc3b2bb43847125f5
Daniel Fireman <danielfireman@gmail.com>
2015-10-01 16:21:29 -0300

    First version of the TODO business logic and REST entry point handling.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,78 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/danielfireman/vvt/todo"
+	"github.com/labstack/echo"
+
+	mw "github.com/labstack/echo/middleware"
+)
+
+var port = flag.Int("port", 8999, "Port to listen.")
+
+type item struct {
+	Desc string
+}
 
 func main() {
-	fmt.Println("FOO")
+	flag.Parse()
+	// Configuring server.
+	e := echo.New()
+	e.Use(mw.Logger())
+	e.Use(mw.Recover())
+
+	// Creating new todo store.
+	s := todo.NewStore()
+
+	// Handlers.
+	e.Post("/todo", func(c *echo.Context) error {
+		var t item
+		if err := c.Bind(&t); err != nil {
+			return c.JSON(http.StatusPreconditionFailed, err.Error())
+		}
+		s.Add(t.Desc)
+		return c.JSON(http.StatusCreated, t)
+	})
+
+	e.Get("/todo/:id", func(c *echo.Context) error {
+		id, err := getID(c)
+		if err != nil {
+			return c.JSON(http.StatusPreconditionFailed, err.Error())
+		}
+		t, err := s.Get(id)
+		if err != nil {
+			return c.JSON(http.StatusNotFound, err.Error())
+		}
+		return c.JSON(http.StatusOK, t)
+	})
+
+	e.Delete("/todo/:id", func(c *echo.Context) error {
+		id, err := getID(c)
+		if err != nil {
+			return c.JSON(http.StatusPreconditionFailed, err.Error())
+		}
+		if err := s.Delete(id); err != nil {
+			return c.JSON(http.StatusNotFound, err.Error())
+		}
+		return c.NoContent(http.StatusNoContent)
+	})
+
+	e.Get("/todolist", func(c *echo.Context) error {
+		return c.JSON(http.StatusOK, s.List())
+	})
+	fmt.Printf("Server listening at localhost:%d\n", *port)
+	e.Run(fmt.Sprintf(":%d", *port))
+}
+
+func getID(c *echo.Context) (int, error) {
+	param := c.Param("id")
+	id, err := strconv.Atoi(param)
+	if err != nil {
+		return 0, fmt.Errorf("Invalid parameter: %v", param)
+	}
+	return id, nil
 }
--- todo/todo.go
+++ todo/todo.go
@@ -1,1 +1,49 @@
+// Package todos handles todo simple string todo list stores.
 package todo
+
+import "errors"
+
+// Creates a new todo item store.
+func NewStore() *store {
+	return &store{}
+}
+
+// todo item store.
+type store struct {
+	content []string
+}
+
+// Adds a todo item to the store.
+func (s *store) Add(i string) error {
+	if len(i) == 0 {
+		return errors.New("Invalid item.")
+	}
+	s.content = append(s.content, i)
+	return nil
+}
+
+// Returns a copy of the todo items stored.
+func (s *store) List() []string {
+	c := make([]string, len(s.content))
+	copy(c, s.content)
+	return c
+}
+
+// Returns the todo item at the nth position in the store or err if there is no
+// item at the specified position.
+func (s *store) Get(n int) (string, error) {
+	if n < 0 || n >= len(s.content) {
+		return "", errors.New("Invalid position.")
+	}
+	return s.content[n], nil
+}
+
+// Deletes the specified item. Returns an error if there is no item stored at
+// the specified position.
+func (s *store) Delete(n int) error {
+	if n < 0 || n >= len(s.content) {
+		return errors.New("Invalid position.")
+	}
+	s.content = append(s.content[:n], s.content[n+1:]...)
+	return nil
+}
