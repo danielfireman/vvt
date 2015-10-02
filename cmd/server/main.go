c0b957094774bc0e5e101b9e6bb11365f4b28897
Daniel Fireman <danielfireman@gmail.com>
2015-10-02 19:09:46 -0300

    Removing Delete handler from main.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/danielfireman/vvt/todo"
 	"github.com/labstack/echo"
@@ -38,41 +37,9 @@ func main() {
 		return c.JSON(http.StatusCreated, t)
 	})
 
-	e.Get("/todo/:id", func(c *echo.Context) error {
-		id, err := getID(c)
-		if err != nil {
-			return c.JSON(http.StatusPreconditionFailed, err.Error())
-		}
-		t, err := s.Get(id)
-		if err != nil {
-			return c.JSON(http.StatusNotFound, err.Error())
-		}
-		return c.JSON(http.StatusOK, t)
-	})
-
-	e.Delete("/todo/:id", func(c *echo.Context) error {
-		id, err := getID(c)
-		if err != nil {
-			return c.JSON(http.StatusPreconditionFailed, err.Error())
-		}
-		if err := s.Delete(id); err != nil {
-			return c.JSON(http.StatusNotFound, err.Error())
-		}
-		return c.NoContent(http.StatusNoContent)
-	})
-
-	e.Get("/todolist", func(c *echo.Context) error {
+	e.Get("/todo", func(c *echo.Context) error {
 		return c.JSON(http.StatusOK, s.List())
 	})
 	fmt.Printf("Server listening at localhost:%d\n", *port)
 	e.Run(fmt.Sprintf(":%d", *port))
-}
-
-func getID(c *echo.Context) (int, error) {
-	param := c.Param("id")
-	id, err := strconv.Atoi(param)
-	if err != nil {
-		return 0, fmt.Errorf("Invalid parameter: %v", param)
-	}
-	return id, nil
 }
