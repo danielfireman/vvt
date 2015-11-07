e893ee8344394a07c314cd600ce335b0eec44e40
Daniel Fireman <danielfireman@gmail.com>
2015-11-07 16:15:39 -0300

    Corrigindo tudo

--- todo/todo.go
+++ todo/todo.go
@@ -20,13 +20,12 @@ type store struct {
 
 // Adds a todo item to the store.
 func (s *store) Add(i *item) error {
-	return errors.New("Invalid item.")
-	/*if len(i.Desc) == 0 {
+	if len(i.Desc) == 0 {
 		return errors.New("Invalid item.")
 	} else {
 		s.content = append(s.content, i)
 		return nil
-	}*/
+	}
 }
 
 // Returns a copy of the todo items stored.
